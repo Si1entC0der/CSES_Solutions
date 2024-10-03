ad89fcb03fd97ded3a4ec1bca8d79068363e1b25
Si1entC0der <shamim.sust.cse.32@gmail.com>
2024-10-03 15:41:25 +0600

    Solved

---
+++ src/1730.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve() {
+    ll n, x_or = 0;
+    cin >> n;
+    vector<ll> v(n);
+    for (auto& it : v) cin >> it, x_or ^= it;
+    x_or ? cout << "first\n" : cout << "second\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    ll t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
